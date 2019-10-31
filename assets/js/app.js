0b811bc16984ab0d9a9109d3cf408b7383bda3cc
jun3pr <jun3pr@hotmail.com>
2019-10-30 19:50:16 -0500

    js

--- assets/js/app.js
+++ assets/js/app.js
@@ -5,13 +5,28 @@ $( document ).ready(function() {
     
     function displayGifButtons() {
         $("#gifButtonsView").empty();
-        for (let i = 0; i < topic.length; i++) {
-        let gifButton = $("<button>");
+            for (let i = 0; i < topic.length; i++) {
+                let gifButton = $("<button>");
         
-        gifButton.addClass("");
+        gifButton.addClass("shows");
         gifButton.addClass("btn btn-primary")
         gifButton.attr("data-name", topic[i]);
         gifButton.text(topic[i]);
+       
         $("#gifButtonsView").append(gifButton);
-        }
-        }
+        }}
+
+        //function to add new button
+    function addNewButton() {
+    $("#addGif").on("click", function() {
+   
+    let shows = $("#topicInput").val().trim();
+        if (shows == ""){
+            return false;//no blank buttons
+            }
+
+    topic.push(shows);
+        displayGifButtons();
+            return false;
+            });
+            }
